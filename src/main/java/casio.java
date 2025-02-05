1e68854159bb4201f403b5ff54f290407323eb95
Louis Tai <142078052+louistaii@users.noreply.github.com>
2025-02-05 22:29:15 +0800

    Add greeting

---
+++ src/main/java/casio.java
@@ -0,0 +1,12 @@
+public class Duke {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________"
+                + "\nHello! I'm Casio\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________"
+                + "\nBye. Hope to see you again soon!\n\n"
+                +"____________________________________________________________";
+
+        System.out.println(greeting);
+    }
+}
