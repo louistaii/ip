668a44c299ab996575a930bbe2e3b8e3d5f8f423
Louis Tai <142078052+louistaii@users.noreply.github.com>
2025-02-06 01:05:41 +0800

    Add multiple task types
    now supports different task types (todo, deadline, event) and their respective dates

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,13 +2,29 @@ public class Task
 {
     protected String description;
     protected boolean isDone;
+    protected String type;
+    protected String date;
 
-    public Task(String description) {
+    public Task(String description, String type) {
         this.description = description;
         this.isDone = false;
+        this.type = type;
+        this.date = "";
     }
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
+    }
+    public String getTypeIcon() {
+        if (type == "todo"){
+            return "T";
+        }
+        if (type == "event"){
+            return "E";
+        }
+        if (type == "deadline"){
+            return "D";
+        }
+        return " ";
     }
 }
